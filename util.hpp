6f97f8ba47fa1027825c29c23ab57b1d7dcafb1b
Ryan Rice <ryanr1230@gmail.com>
2016-11-24 23:32:47 -0600

    util.hpp

---
+++ util.hpp
@@ -0,0 +1,86 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Module.h>
+
+llvm::Module makeLLVMModule();
+llvm::Function* func_error ;
+llvm::Function* func_printf ;
+llvm::Function* func_exit ;
+llvm::Function* func_makeInt ;
+llvm::Function* func_malloc ;
+llvm::Function* func_makeString ;
+llvm::Function* func_makeList ;
+llvm::Function* func_readString ;
+llvm::Function* func_getline ;
+llvm::Function* func_readInt ;
+llvm::Function* func_atoi ;
+llvm::Function* func_add_str ;
+llvm::Function* func_strlen ;
+llvm::Function* func_strcpy ;
+llvm::Function* func_strcat ;
+llvm::Function* func_add_int ;
+llvm::Function* func_add_any ;
+llvm::Function* func_eq_int ;
+llvm::Function* func_eq_str ;
+llvm::Function* func_strcmp ;
+llvm::Function* func_eq_any ;
+llvm::Function* func_neq_int ;
+llvm::Function* func_neq_str ;
+llvm::Function* func_neq_any ;
+llvm::Function* func_sub_int ;
+llvm::Function* func_gt_int ;
+llvm::Function* func_geq_int ;
+llvm::Function* func_lt_int ;
+llvm::Function* func_leq_int ;
+llvm::Function* func_div_int ;
+llvm::Function* func_mul_int ;
+llvm::Function* func_and_int ;
+llvm::Function* func_or_int ;
+llvm::Function* func_sub_any ;
+llvm::Function* func_gt_any ;
+llvm::Function* func_geq_any ;
+llvm::Function* func_lt_any ;
+llvm::Function* func_leq_any ;
+llvm::Function* func_div_any ;
+llvm::Function* func_mul_any ;
+llvm::Function* func_and_any ;
+llvm::Function* func_or_any ;
+llvm::Function* func_cons_any ;
+llvm::Function* func_hd ;
+llvm::Function* func_tl ;
+llvm::Function* func_hd_any ;
+llvm::Function* func_tl_any ;
+llvm::Function* func_is_nil ;
+llvm::Function* func_print_int ;
+llvm::Function* func_print_str ;
+llvm::Function* func_lt_int ;
+llvm::Function* func_leq_int ;
+llvm::Function* func_div_int ;
+llvm::Function* func_mul_int ;
+llvm::Function* func_and_int ;
+llvm::Function* func_or_int ;
+llvm::Function* func_sub_any ;
+llvm::Function* func_gt_any ;
+llvm::Function* func_geq_any ;
+llvm::Function* func_lt_any ;
+llvm::Function* func_leq_any ;
+llvm::Function* func_div_any ;
+llvm::Function* func_mul_any ;
+llvm::Function* func_and_any ;
+llvm::Function* func_or_any ;
+llvm::Function* func_cons_any ;
+llvm::Function* func_hd ;
+llvm::Function* func_tl ;
+llvm::Function* func_hd_any ;
+llvm::Function* func_tl_any ;
+llvm::Function* func_is_nil ;
+llvm::Function* func_print_int ;
+llvm::Function* func_print_str ;
+llvm::Function* func_print_list ;
+llvm::Function* func_free ;
+llvm::Function* func_print_any ;
+
+
+#endif
